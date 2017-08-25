3058a45955e86f7241149ba6190ec656ff624149
SinethembaDlova <dSinethemba@gmail.com>
2017-08-25 15:46:25 +0200

    first commit

---
+++ Greeting.java
@@ -0,0 +1,13 @@
+import java.io.Console;
+
+public class Greeting {
+  public static void main(String[] args) {
+    //Console console = System.console();
+
+    String name = "Garreth";
+    int age = 23;
+
+  //  System.out.print("Hello World!");
+    System.out.print("Hello " + name + "you are " + age + "years old");
+  }
+}
